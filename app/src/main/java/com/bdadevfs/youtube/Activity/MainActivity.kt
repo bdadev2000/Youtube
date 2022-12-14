d5b381c27c1233077275e9139cbac8873d892731
ducanh <anhbd@its-global.vn>
2022-12-15 06:32:51 +0700

    init project

---
+++ app/src/main/java/com/bdadevfs/youtube/Activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bdadevfs.youtube.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bdadevfs.youtube.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
