6d0f8edc4754d3a342603d3cd3fb4252965b4632
ducanh <anhbd@its-global.vn>
2022-12-19 06:20:01 +0700

    load video in ExoMedia

--- app/src/main/java/com/bdadevfs/youtube/Adapter/VideoAdapter.kt
+++ app/src/main/java/com/bdadevfs/youtube/Adapter/VideoAdapter.kt
@@ -1,9 +1,11 @@
 package com.bdadevfs.youtube.Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bdadevfs.youtube.Activity.VideoActivity
 import com.bdadevfs.youtube.Model.VideoResponse
 import com.bdadevfs.youtube.databinding.ItemVideoBinding
 import com.bumptech.glide.Glide
@@ -24,6 +26,11 @@ class VideoAdapter : RecyclerView.Adapter<VideoAdapter.VideoViewHolder>(){
 
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
       Glide.with(holder.itemView).load(list[position].image).into(holder.binding.imageView)
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context,VideoActivity::class.java)
+            intent.putExtra("URL",list[position].video_files[0].link)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
